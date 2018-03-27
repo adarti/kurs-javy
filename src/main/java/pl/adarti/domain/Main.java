9c8409302f7ca4678ccfd06df7e54a7c68155471
Dariusz Stankowski <dstankowski@op.pl>
2018-03-27 11:41:34 +0200

    solutions for lesson#2
    add message

--- src/main/java/pl/adarti/domain/Main.java
+++ src/main/java/pl/adarti/domain/Main.java
@@ -1,8 +1,15 @@
 package pl.adarti.domain;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Sara");
+        //read from screen
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("write a cat name");
+        String catName = scanner.nextLine();
+        Cat cat = new Cat(catName);
         System.out.println(cat.introduceYourself());
     }
 }
